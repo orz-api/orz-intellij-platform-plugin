38ddb48a6079fc2ae1c8fd882fd69dc5f63a834a
GitHub Action <action@github.com>
2023-10-11 09:41:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/reset7523/orzintellijplatformplugin/MyBundle.kt
+++ src/main/kotlin/com/github/reset7523/orzintellijplatformplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.reset7523.orzintellijplatformplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/reset7523/orzintellijplatformplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/reset7523/orzintellijplatformplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.reset7523.orzintellijplatformplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
